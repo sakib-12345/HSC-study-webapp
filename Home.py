6db726c5526fd6b9d77d6072a33dc258c965f60e
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-19 22:50:57 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,16 +1,12 @@
 import streamlit as st
 
-st.write("Home Page")
+st.header("Home")
 
-st.write("study material will be updated soon...")
+st.write("> study material will be updated soon...")
 
-with st.expander("ICT"):
+with st.expander("Quick View"):
     st.markdown("[ICT PAGE](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#ict-page)")
-    st.markdown("[CHAPTER-01](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#chapter-01)")
-    st.markdown("[CHAPTER-02](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#chapter-02)")
-    st.markdown("[CHAPTER-03](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#chapter-03)")
-    st.markdown("[CHAPTER-04](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#chapter-04)")
-    st.markdown("[CHAPTER-05](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#chapter-05)")
-    st.markdown("[CHAPTER-06](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#chapter-06)")
+   
+
 
 
