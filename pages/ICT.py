a060527716fbb54e6fc23762e6c4cfc38b21c920
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-19 23:48:10 +0600

    Update ICT.py

--- pages/ICT.py
+++ pages/ICT.py
@@ -25,5 +25,7 @@ with t1:
 with t2:
     for ch, ur in data.items():
         with st.expander(ch):
+            st.markdown(f"(Link veiw)[{ur[1]}]")
             st.markdown(pdf_view(ur[1]), unsafe_allow_html=True)
+
 
