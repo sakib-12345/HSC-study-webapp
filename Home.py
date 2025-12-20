9534dd1d96f62b0ec85d283d5e2e1cf7d86e99ab
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-20 21:26:51 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,8 +5,9 @@ st.header("Home")
 st.write("> study material will be updated soon...")
 
 with st.expander("Quick View"):
-    st.markdown("[ICT PAGE](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#ict-page)")
-   
+    st.markdown("[ICT PAGE](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#ict-review)")
+    st.markdown("[MATH PAGE](https://hsc-study-webapp-by-sakib.streamlit.app/MATH#math-review)")
+
 
 
 
