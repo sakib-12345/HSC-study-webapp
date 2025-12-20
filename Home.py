f3c7283f57d4110808585bb10b03c0f82c624931
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-20 22:18:19 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import helper
+from helper import ani_head, side_note, social_links
 
 st.markdown(ani_head(), unsafe_allow_html=True)
 
@@ -12,6 +12,7 @@ with st.expander("Quick View"):
 m = "The video and pdf in this app are pulled from youtube and google drive.<br> They are free content and available in their owner's youtube channel and drive."
 st.markdown(side_note("About this app", ), unsafe_allow_html=True)
 st.markdown(social_links(), unsafe_allow_html=True)
+
 
 
 
