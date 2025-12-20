6eaa5fa9752354039cef89c1145fe1ae76ce6d62
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-20 21:47:27 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,15 @@
 import streamlit as st
+from helper import ani_head
 
 st.header("Home")
+ani_head()
 
 st.write("> study material will be updated soon...")
 
 with st.expander("Quick View"):
     st.markdown("[ICT PAGE](https://hsc-study-webapp-by-sakib.streamlit.app/ICT#ict-review)")
     st.markdown("[MATH PAGE](https://hsc-study-webapp-by-sakib.streamlit.app/MATH#math-review)")
+
 
 
 
