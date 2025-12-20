1d4de5e3a406b306ec5b2936e42441ec53ddfa65
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-20 22:34:02 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,24 @@
 import streamlit as st
 from helper import ani_head, side_note, social_links
 
+
+st.set_page_config(
+    page_title="Hsc Study",
+    page_icon="📈",
+    layout="wide"
+)
+ hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            
+            </style>
+            """
+    
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+
 st.markdown(ani_head(), unsafe_allow_html=True)
 
 st.write("> study material will be updated soon...")
@@ -12,6 +30,11 @@ with st.expander("Quick View"):
 st.markdown(side_note(), unsafe_allow_html=True)
 st.markdown(social_links(), unsafe_allow_html=True)
 
+st.markdown(
+            '<div style="text-align: center; color: grey;">v1.0.0</div>',
+            unsafe_allow_html=True
+
+           ) 
 
 
 
