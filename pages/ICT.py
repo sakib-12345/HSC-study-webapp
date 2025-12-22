9ca3a426bb17eb0efb4dcafe3738b35c6dfb4807
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-22 21:56:53 +0600

    Update ICT.py

--- pages/ICT.py
+++ pages/ICT.py
@@ -1,13 +1,8 @@
 import streamlit as st
 from data import ict
-from helper import pdf_view, check_auth
+from helper import pdf_view, check_auth, Page
 
-st.set_page_config(
-    page_title="Hsc Study",
-    page_icon="📈",
-    layout="wide"
-)
-
+Page()
 check_auth()
 hide_st_style = """
             <style>
@@ -37,6 +32,7 @@ with t2:
         with st.expander(ch):
             st.markdown(f"[⛶ Full veiw]({ur[1]})", unsafe_allow_html=True)
             st.markdown(pdf_view(ur[1]), unsafe_allow_html=True)
+
 
 
 
