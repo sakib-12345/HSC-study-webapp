30a52aab30c47db99959cb8733bc09b166beba41
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-23 00:43:24 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -39,45 +39,7 @@ with st.container(border=True):
     with a4:
         st.page_link("pages/ICT.py", label=f"![icon]({PNG_URL}) ICT PAGE")
 
-
-
-
 st.write("")
-st.write("")
-st.write("")
-st.write("")
-
-st.subheader("Search here", divider="violet")
-st.markdown('<div style="color: grey;">Only Math, Biology, Chemistry and ICT for now. Others coming soon....</div>',unsafe_allow_html=True)
-sub = st.selectbox("📘 Select subject", subjects.keys())
-
-c1, c2 = st.columns([1, 1])
-with c1:
-    paper = st.selectbox("📄 Select paper", subjects[sub].keys())
-with c2:
-    chapter = st.selectbox("Select Chapter",["All Chapters"] + list(subjects[sub][paper].keys()))
-
-if st.button("Search"):
-    if chapter == "All Chapters":
-        items = subjects[sub][paper].items()
-    else:
-        items = [(chapter, subjects[sub][paper][chapter])]
-
-    with st.container(height=500):
-        st.markdown(f"#### 📚 {sub} - {paper} - {chapter}")
-        for chap, links in items:
-            with st.container(border=True):
-                col1, col2 = st.columns([2, 3])
-
-                with col1:
-                    st.video(links[0])
-
-                with col2:
-                    st.markdown(f"### {chap}")
-                    st.markdown(f"[Open PDF]({links[1]})")
-else:
-    with st.container(height=500):
-        st.markdown('<div style="text-align: center; color: grey;">No search result</div>',unsafe_allow_html=True)
 
 st.write("")
 st.write("")
@@ -96,6 +58,7 @@ st.markdown(
             unsafe_allow_html=True
 
            ) 
+
 
 
 
