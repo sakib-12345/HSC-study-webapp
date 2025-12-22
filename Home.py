159fce7b71b3ebfe258a9a5e3dc02ecdf9fd4a5a
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-23 00:06:53 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from data import subjects
 from helper import ani_head, side_note, social_links, check_auth, Page, side_note_2
 from streamlit.components.v1 import html
-st.header("")
+st.text_input("top_focus", label_visibility="collapsed", key="top_focus_trick"
 html("<script>window.parent.window.scrollTo(0,0);</script>", height=0)
 st.html('<div id="top"></div>') 
 
@@ -135,6 +135,7 @@ st.markdown(
             unsafe_allow_html=True
 
            ) 
+
 
 
 
