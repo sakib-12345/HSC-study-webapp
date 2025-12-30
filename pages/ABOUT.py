505a8e6688367b0caf1ad164215e12c707caa98e
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-31 00:20:21 +0600

    Create ABOUT.py

---
+++ pages/ABOUT.py
@@ -0,0 +1,62 @@
+import streamlit as st
+
+st.markdown("""
+
+
+# 📚 HSC Study WebApp
+**Author:** Sakib Hossain Tahmid  
+*Try the App:* [Click Here](https://hsc-study-webapp-by-sakib.streamlit.app/)
+
+<p align="center">
+  <img src="https://github.com/sakib-12345/HSC-study-webapp/blob/main/webapp_icon.png?raw=true" alt="App Screenshot" width="200">
+</p>
+
+A distraction-free study web app for HSC students, built with **Python** and **Streamlit**.  
+Access free **one-shot classes** and their PDFs in one place, organized by subject.
+
+
+
+## 🚀 Features
+
+### 👨‍🎓 User Features
+- Access **free HSC classes** in one place  
+- Search or navigate to **dedicated subject pages** via the sidebar  
+- View and download **PDF study materials**  
+- Organized and distraction-free interface  
+
+### 📚 Content
+- Videos and PDFs are sourced from **YouTube** and **Google Drive**  
+- All content is **free and publicly available**  
+- Video views are counted on the **original creator’s YouTube channel**  
+- Supports focused, efficient learning  
+
+
+
+## 🛠️ Tech Stack
+- **Python**  
+- **Streamlit**  
+- **Google Drive / YouTube (for content)**  
+
+
+## 🔧 Setup
+
+#### 1.Clone
+```bash
+https://github.com/sakib-12345/chat-app-with-google-sheet.git
+```
+#### 2.Open folder
+```bash
+cd chat-app-with-google-sheet
+```
+#### 3.Install Libraries
+```bash
+pip install -r requirements.txt
+```
+#### 4.Run it (Locally)
+```bash
+streamlit run chat.py
+```
+## License
+- This project is licensed under the MIT License. You can use, copy, and modify it freely as long as you include the original license.
+
+""", unsafe_allow_html=True)
