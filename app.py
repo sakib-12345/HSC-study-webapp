72525eb59c0f5a839e693bee11316e455b76bf2e
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2025-12-31 22:55:19 +0600

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ pg = st.navigation(
 pg.run()
 
 
-st.markdown(social_links(), unsafe_allow_html=True)
+st.sidebar.markdown(social_links(), unsafe_allow_html=True)
 
 year = datetime.now().year
 st.markdown(f"""<p style="text-align:center; font-size:14px; color:gray;">© {year} Sakib Hossain Tahmid. All rights reserved.</p>""", unsafe_allow_html=True)
