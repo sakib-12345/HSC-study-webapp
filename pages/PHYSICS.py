5dd0461f0f71734fa8a0563275130d01cfdca9f1
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2026-01-01 20:55:45 +0600

    Create PHYSICS.py

---
+++ pages/PHYSICS.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.header("Physics Review")
+
+
+t3, t4 = st.tabs(["1st Paper", "2nd Paper"])
+
+with t3:
+    pass
+
+with t4:
+    pass
+
+st.markdown('<div style="text-align: center; color: grey;">No Videos Available For Now</div>',unsafe_allow_html=True)
