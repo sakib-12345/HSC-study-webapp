a0b70ca86b0e5b37ebf2c7546166bcc4a2ce5147
Sakib Hossain Tahmid <sakibhossaintahmid@gmail.com>
2026-01-01 22:52:46 +0600

    Update CODE.py

--- pages/CODE.py
+++ pages/CODE.py
@@ -4,7 +4,34 @@ from pathlib import Path
 st.title("Project Source Code")
 
 st.markdown("OPEN IN GITHUB [CLICK HERE](https://github.com/sakib-12345/HSC-study-webapp)", unsafe_allow_html=True)
-
+st.write("")
+st.write("")
+st.write("### 🔧 File Structure")
+st.markdown("""
+```
+HSC-study-webapp/
+├── .streamlit/
+│   └── config.toml
+├── License
+├── README.md
+├── app.py
+├── click.png
+├── data.py
+├── helper.py
+├── pages/
+│   ├── ABOUT.py
+│   ├── BIOLOGY.py
+│   ├── CHEMISTRY.py
+│   ├── CODE.py
+│   ├── HOME.py
+│   ├── ICT.py
+│   ├── MATH.py
+│   ├── PHYSICS.py
+│   └── SEARCH.py
+├── requirements.txt
+└── webapp_icon.png
+```
+""", unsafe_allow_html=True)
 ROOT = Path(__file__).parent.parent  # adjust if this file is inside /pages
 
 EXCLUDE = {".streamlit", "venv", "__pycache__", ".git"}
@@ -19,9 +46,8 @@ st.write("")
 st.write("")
 st.write("")
 for file in files:
-    st.markdown(f"### 📄 {file.relative_to(ROOT)}")
     code = file.read_text(encoding="utf-8", errors="ignore")
-    with st.expander("CODE (click here)", expanded=False):
+    with st.expander(f"### 📄 {file.relative_to(ROOT)}", expanded=False):
         st.code(code, language="python", line_numbers=True)
 
     
